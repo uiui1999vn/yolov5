9771825710c32604a4540fe4ca02a6ac1d8a4f40
awl-thanh-nguyen <thanh.nguyen@awl.com.vn>
2020-08-05 16:48:56 +0700

    add yolov5l_ceot_hand.yaml

--- run_train.sh
+++ run_train.sh
@@ -4,7 +4,7 @@ rm  ./datasets/CEOT_hand_yolo/labels/*.cache
 
 python train.py \
         --img 640 \
-        --cfg ./models/yolov5l.yaml \
+        --cfg ./models/yolov5l_ceot_hand.yaml \
         --batch 16 \
         --epochs 50 \
         --data ./data/ceot_hand.yaml \
