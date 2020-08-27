9201bbdc82f68e1aada65e7f4bcf4d5a55ff71b3
edwardnguyen1705 <thanhng1985@gmail.com>
2020-08-27 08:09:56 +0700

    change batch size 96

--- run_train.sh
+++ run_train.sh
@@ -18,7 +18,7 @@ rm  /home/thanh_nguyen/Datasets/COCO2017_Person/labels/*.cache
 python train.py \
         --img 512 \
         --cfg ./models/yolov5s.yaml \
-        --batch 128 \
+        --batch 96 \
         --epochs 200 \
         --data ./data/coco_person.yaml \
         --single-cls \
